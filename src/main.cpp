e3658139587626cc4d7830d10e2c625b76502b36
Willyan <willyansilvagomes2020@gmail.com>
2023-04-09 21:02:43 -0300

    host

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@
 #include <string>
 
 // Substitua com as suas informações de rede Wi-Fi
-// const char* ssid = "Willyan's Galaxy S20 FE";
-// const char* password = "123456789";
+// const char* ssid = "brisa-2908314";
+// const char* password = "dqszdq8h";
 const char* ssid = "Willyan's Galaxy S20 FE";
 const char* password = "123456789";
 
@@ -46,18 +46,20 @@ void sendRequest(String chipNumber) {
   String jsonStr;
   serializeJson(jsonPayload, jsonStr);
 
-  int httpCode = http.POST(jsonStr);
+  if (WiFi.status() == WL_CONNECTED) {
+    int httpCode = http.POST(jsonStr);
 
-  // Verifique se a solicitação foi bem-sucedida
-  if (httpCode > 0) {
-    Serial.printf("[HTTP] Response Code: %d\n", httpCode);
-    String response = http.getString();
-    Serial.println(response);
-  } else {
-    Serial.printf("[HTTP] Bad Request. Error Code: %s\n", http.errorToString(httpCode).c_str());
+    // Verifique se a solicitação foi bem-sucedida
+    if (httpCode > 0) {
+      Serial.printf("[HTTP] Response Code: %d\n", httpCode);
+      String response = http.getString();
+      Serial.println(response);
+    } else {
+      Serial.printf("[HTTP] Bad Request. Error Code: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
   }
-
-  http.end();
 }
 
 void loop() {
@@ -65,13 +67,23 @@ void loop() {
   //if (digitalRead(pin34) == HIGH) {
     // Faça a requisição HTTP POST
      //}
+
+while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Perda de conexão!");
+    WiFi.begin(ssid, password);
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("Conectado novamente!");
+    }
+  }
+
 String numbers[] = {"58479826", "15489823", "58479826", "13482823", "58479826","13482823", "78591204"};
 
 for (String number : numbers) {
   sendRequest(number);
-  delay(240000);
+  delay(60000);
 }
 
-  delay(1200000);
+  delay(1800000);
   // Nada a fazer no loop principal
 }
